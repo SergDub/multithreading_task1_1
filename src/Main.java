6bfd176f79c8db57e45ea766dea107fd4ead6236
Сергей Дубовский <svdub@mail.ru>
2023-03-01 14:24:18 +0300

    Вычисление максимума

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,37 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         String[] texts = new String[25];
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("aab", 30_000);
         }
-        List<Thread> threadList = new ArrayList<>();
+        List<Future> futureList = new ArrayList<>();
+        int maxValue = 0;
+        ExecutorService threadPool = Executors.newFixedThreadPool(texts.length);
 
         long startTs = System.currentTimeMillis();
 
         for (String text : texts) {
-            threadList.add(new Thread(
-                    () -> {
-                        int maxSize = 0;
-                        for (int i = 0; i < text.length(); i++) {
-                            for (int j = 0; j < text.length(); j++) {
-                                if (i >= j) {
-                                    continue;
-                                }
-                                boolean bFound = false;
-                                for (int k = i; k < j; k++) {
-                                    if (text.charAt(k) == 'b') {
-                                        bFound = true;
-                                        break;
-                                    }
-                                }
-                                if (!bFound && maxSize < j - i) {
-                                    maxSize = j - i;
-                                }
-                            }
-                        }
-                        System.out.println(text.substring(0, 100) + "->" + maxSize);
-                    }
-            ));
+            futureList.add(threadPool.submit(new CallableTask(text)));
         }
 
-        for (Thread thread : threadList) {
-            thread.start();
+        for (Future future : futureList) {
+            maxValue = Math.max((int) future.get(), maxValue);
         }
-        for (Thread thread : threadList) {
-            thread.join();
-        }
-
-
+        System.out.println(" Максимальный интервал значений " + maxValue);
+        threadPool.shutdown();
         long endTS = System.currentTimeMillis();
 
         System.out.println("Time: " + (endTS - startTs) + "ms");
     }
-
     public static String generateText(String letters, int length) {
         Random random = new Random();
         StringBuilder text = new StringBuilder();
@@ -59,6 +40,4 @@ public class Main {
         }
         return text.toString();
     }
-
-
 }
